bec2db6b9a7f9245bb094574a6eb359b9149bafc
MaRuidong <2798640402@qq.com>
2019-02-28 16:28:38 +0800

    算法

---
+++ 第1章/quickSort.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 28 16:18:41 2019
+
+@author: PC-CONFIG
+"""
+
+#从小到大快速排序
+def quickSort():
+    A = [6,4,8,9,0,3,7,2]
+    
+    for i in range(1,len(A)):
+        key = A[i]
+        j = i-1
+        while j>=0 and A[j] > key:
+            A[j+1] = A[j]
+            j = j-1
+        A[j+1] = key
+    print(A)
+    
+quickSort()    
