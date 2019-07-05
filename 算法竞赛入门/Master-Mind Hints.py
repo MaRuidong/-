aaed73a3bc1ab1ae90fbba58551e45a9396cd9fb
MaRuidong <2798640402@qq.com>
2019-07-05 22:36:05 +0800

    猜数字游戏

---
+++ 算法竞赛入门/Master-Mind Hints.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: PC-CONFIG
+@license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+@contact: marudong951@gmail.com
+@software: PC-CONFIG
+@file: Master-Mind Hints.py
+@time: 2019/7/5 21:54
+@desc:
+'''
+
---
+++ 算法竞赛入门/pailie.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: PC-CONFIG
+@license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+@contact: marudong951@gmail.com
+@software: PC-CONFIG
+@file: pailie.py
+@time: 2019/7/4 21:41
+@desc:
+'''
